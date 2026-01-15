ff441b911b0b7bad0a98138ad82b457902df2728
Sevket Gökay <sevketgokay@gmail.com>
2026-01-15 16:03:20 +0100

    enhance with lombok annotations

--- ocpp-custom/src/main/java/de/rwth/idsg/ocpp/jaxb/CustomAnnotator.java
+++ ocpp-custom/src/main/java/de/rwth/idsg/ocpp/jaxb/CustomAnnotator.java
@@ -1,13 +1,28 @@
 package de.rwth.idsg.ocpp.jaxb;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JFieldVar;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.joda.time.DateTime;
 import org.jsonschema2pojo.AbstractAnnotator;
 
 public class CustomAnnotator extends AbstractAnnotator {
+
+    @Override
+    public void typeInfo(JDefinedClass clazz, JsonNode schema) {
+        super.typeInfo(clazz, schema);
+
+        clazz.annotate(ToString.class);
+        clazz.annotate(Getter.class);
+        clazz.annotate(Setter.class);
+        clazz.annotate(EqualsAndHashCode.class);
+    }
 
     @Override
     public void propertyField(JFieldVar field, JDefinedClass clazz,
