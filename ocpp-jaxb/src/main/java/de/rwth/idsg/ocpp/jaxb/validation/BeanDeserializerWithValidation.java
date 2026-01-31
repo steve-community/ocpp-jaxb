6d3c930a517a194e3f9d6f9a854d818feeae3b38
Sevket Gökay <sevketgokay@gmail.com>
2026-01-31 17:42:45 +0100

    refactor deser impl for symmetry with the ser approach

--- ocpp-jaxb/src/main/java/de/rwth/idsg/ocpp/jaxb/validation/BeanDeserializerWithValidation.java
+++ ocpp-jaxb/src/main/java/de/rwth/idsg/ocpp/jaxb/validation/BeanDeserializerWithValidation.java
@@ -1,9 +1,11 @@
 package de.rwth.idsg.ocpp.jaxb.validation;
 
+import lombok.RequiredArgsConstructor;
 import tools.jackson.core.JacksonException;
 import tools.jackson.core.JsonParser;
 import tools.jackson.databind.DeserializationContext;
-import tools.jackson.databind.deser.bean.BeanDeserializer;
+import tools.jackson.databind.ValueDeserializer;
+import tools.jackson.databind.deser.bean.BeanDeserializerBase;
 
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validator;
@@ -11,18 +13,15 @@ import jakarta.validation.Validator;
 /**
  * https://www.baeldung.com/java-object-validation-deserialization
  */
-public class BeanDeserializerWithValidation extends BeanDeserializer {
+@RequiredArgsConstructor
+public class BeanDeserializerWithValidation extends ValueDeserializer<Object> {
 
+    private final BeanDeserializerBase delegate;
     private final Validator validator;
-
-    public BeanDeserializerWithValidation(BeanDeserializer src, Validator validator) {
-        super(src);
-        this.validator = validator;
-    }
 
     @Override
     public Object deserialize(JsonParser p, DeserializationContext ctxt) throws JacksonException {
-        var instance = super.deserialize(p, ctxt);
+        var instance = delegate.deserialize(p, ctxt);
 
         var violations = validator.validate(instance);
         if (!violations.isEmpty()) {
