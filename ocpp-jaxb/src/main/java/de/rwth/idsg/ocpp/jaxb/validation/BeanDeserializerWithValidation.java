eeb48cc9461d731ae289c3ae36eb65e34a26225f
Sevket Gökay <sevketgokay@gmail.com>
2026-01-31 19:11:47 +0100

    refactor to use BeanDeserializerBase, DelegatingDeserializer

--- ocpp-jaxb/src/main/java/de/rwth/idsg/ocpp/jaxb/validation/BeanDeserializerWithValidation.java
+++ ocpp-jaxb/src/main/java/de/rwth/idsg/ocpp/jaxb/validation/BeanDeserializerWithValidation.java
@@ -3,7 +3,9 @@ package de.rwth.idsg.ocpp.jaxb.validation;
 import tools.jackson.core.JacksonException;
 import tools.jackson.core.JsonParser;
 import tools.jackson.databind.DeserializationContext;
-import tools.jackson.databind.deser.bean.BeanDeserializer;
+import tools.jackson.databind.ValueDeserializer;
+import tools.jackson.databind.deser.bean.BeanDeserializerBase;
+import tools.jackson.databind.deser.std.DelegatingDeserializer;
 
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validator;
@@ -11,24 +13,36 @@ import jakarta.validation.Validator;
 /**
  * https://www.baeldung.com/java-object-validation-deserialization
  */
-public class BeanDeserializerWithValidation extends BeanDeserializer {
+public class BeanDeserializerWithValidation extends DelegatingDeserializer {
 
-    private final Validator validator;
-
-    public BeanDeserializerWithValidation(BeanDeserializer src, Validator validator) {
-        super(src);
-        this.validator = validator;
+    public BeanDeserializerWithValidation(BeanDeserializerBase d, Validator validator) {
+        super(create(d, validator));
     }
 
     @Override
-    public Object deserialize(JsonParser p, DeserializationContext ctxt) throws JacksonException {
-        var instance = super.deserialize(p, ctxt);
+    protected ValueDeserializer<?> newDelegatingInstance(ValueDeserializer<?> newDelegatee) {
+        return newDelegatee;
+    }
 
-        var violations = validator.validate(instance);
-        if (!violations.isEmpty()) {
-            throw new ConstraintViolationException(violations);
-        }
+    private static ValueDeserializer<?> create(ValueDeserializer<?> delegate, Validator validator) {
+        return new ValueDeserializer<>() {
 
-        return instance;
+            @Override
+            public Class<?> handledType() {
+                return delegate.handledType(); // otherwise NPE in constructor of DelegatingDeserializer
+            }
+
+            @Override
+            public Object deserialize(JsonParser p, DeserializationContext ctxt) throws JacksonException {
+                var instance = delegate.deserialize(p, ctxt);
+
+                var violations = validator.validate(instance);
+                if (!violations.isEmpty()) {
+                    throw new ConstraintViolationException(violations);
+                }
+
+                return instance;
+            }
+        };
     }
 }
